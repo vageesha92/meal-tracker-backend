cc9a29b61eb5c9fd1fb71a517c0606da6d563741
Vageesha Singh <vageesha92@gmail.com>
2022-01-06 16:58:36 -0600

    helper function for getting meals

---
+++ src/db/getMeals.js
@@ -0,0 +1,7 @@
+import {db} from "./db";
+
+export const getMeals= async()=>{
+    const connection =await db.getConnection();
+    const Meals= await connection.collection(Meals).find({}).toArray();
+  return Meals;
+}
