c7ba41131f76684c7fb805184e83cf66feac6b8a
Vageesha Singh <vageesha92@gmail.com>
2022-01-06 18:28:09 -0600

    parameter of collection method should be string

--- src/db/getMeals.js
+++ src/db/getMeals.js
@@ -2,6 +2,6 @@ import {db} from "./db";
 
 export const getMeals= async()=>{
     const connection =await db.getConnection();
-    const Meals= await connection.collection(Meals).find({}).toArray();
+    const Meals= await connection.collection("Meals").find({}).toArray();
   return Meals;
 }
