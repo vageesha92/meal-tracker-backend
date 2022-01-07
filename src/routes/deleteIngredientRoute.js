c99421e80a6c8c6dd13ae8306c9d78d557396717
Vageesha Singh <vageesha92@gmail.com>
2022-01-07 11:35:13 -0600

    Add to delete ingredients

--- src/routes/deleteIngredientRoute.js
+++ src/routes/deleteIngredientRoute.js
@@ -1,7 +1,12 @@
+import{getPopulatedIngredients, deleteIngredient} from "../db";
+
 export const deleteIngredientRoute={
     method:"delete",
     path:"/ingredients/:name",
     handler: async(req,res)=>{
-
+         const {name}=req.params;
+         await deleteIngredient(name);
+         const updatedIngredients=await getPopulatedIngredients();
+         res.status(200).json(updatedIngredients);
     },
 };
